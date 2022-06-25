1c40cb55a6af04ee5794b7d44945e210dc2602c6
hasansheikh1 <hasanfaysal17@gmail.com>
2022-06-25 17:09:35 +0500

    first

---
+++ func.js
@@ -0,0 +1,15 @@
+// function mysqr(){
+// var x=prompt("Enter your number: ",x);
+// document.getElementById('demo').innerHTML="Square of " + x + " is:" +Math.sqrt(x);
+
+// }
+// function log(){
+//     var y=prompt("Enter your number: ",y);
+//     document.getElementById('demo').innerHTML="Log of " + y + " is:" +Math.log(y);
+    
+//     }
+//     function trig(){
+//         var k=prompt("Enter your number for finding sin and cos: ",k);
+//         document.getElementById('demo').innerHTML="Sin of " + k + " is:" +Math.sin(k * Math.PI/100)+"<br>"+
+//         "cos of "+k+" is:"+ Math.cos(k*Math.PI/100);
+//         }
